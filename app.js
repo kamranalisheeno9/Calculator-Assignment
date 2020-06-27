49220622b8e554ae5169c0e80954928ab91b7782
unknown <kamranalisheeno9@gmail.com>
2020-06-27 12:54:31 +0500

    First Web Application ( Calculator )

---
+++ app.js
@@ -0,0 +1,27 @@
+function clearBtn(){
+    var clearValue=document.getElementById("result")
+    var clearinnerValue=document.getElementById("innerresult")
+    clearValue.value= "";
+    clearinnerValue.value= "";
+
+}
+function number(num){
+    var numberInput=document.getElementById("innerresult")
+    numberInput.value += num;
+}
+function resultEnd(){
+    var resultValue=document.getElementById("innerresult")
+    var resultValueMain=document.getElementById("result")
+    resultValueMain.value=eval(resultValue.value)
+    
+    document.getElementById("innerresult").value =resultValueMain.value;
+    document.getElementById("innerresult").value ="";
+
+
+
+}
+function delNum(){
+    var resultValue=document.getElementById("innerresult")
+    resultValue.value=(resultValue.value).substring(0,(resultValue.value).length - 1)
+}
+
